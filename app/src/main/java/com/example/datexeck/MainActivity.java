8cf62c78adc4c1e26224103f61f4fdd359af5147
nirvikBasnet <nirvik41@gmail.com>
2020-01-07 19:05:18 +1100

    Dashboard UI

--- app/src/main/java/com/example/datexeck/MainActivity.java
+++ app/src/main/java/com/example/datexeck/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.datexeck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView totalItems;
+    Button addButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
-    private void printName(){
-        System.out.println("Nirvik");
-    }
+
 }
