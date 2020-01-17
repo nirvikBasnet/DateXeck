8dba58841d32dbfba0d47e6ea562192ecf9d7838
nirvikBasnet <nirvik41@gmail.com>
2020-01-18 00:36:36 +1100

    Fragments Added To Menu Items

--- app/src/main/java/com/example/datexeck/MainActivity.java
+++ app/src/main/java/com/example/datexeck/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.datexeck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +29,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(itemSelectedListener);
+
+        HomeFragment homeFragment = new HomeFragment();
 
 
 
+    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,homeFragment).commit();
 
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener itemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_list:
+                            selectedFragment = new ListFragment();
+                            break;
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_camera:
+                            selectedFragment = new CameraFragment();
+                            break;
+                        case R.id.nav_notification:
+                            selectedFragment = new NotificationFragment();
+                            break;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+                    return true;
+
+                }
+            };
+
 
 
 
